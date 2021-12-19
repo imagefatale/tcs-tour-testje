143b922a90fb2e9a82724df8189f2d952b078e4d
KVDB <imagefatale@yahoo.com>
2021-12-19 16:30:18 -0500

    Commit changes

---
+++ data.js
@@ -0,0 +1,139 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.4453597841528918,
+        "pitch": 0.4311432269558697,
+        "fov": 1.3797825355761058
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9198682916884096,
+          "pitch": 0.3498599295193241,
+          "rotation": 0,
+          "target": "1-r0020007-32"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3220045132784115,
+          "pitch": 0.10510684539572068,
+          "title": "Another entry<br>",
+          "text": "no photo tour here<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0020007-32",
+      "name": "R0020007-32",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -1.5517459586131999,
+        "pitch": 0.11673948563894676,
+        "fov": 1.3797825355761058
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.989500941100811,
+          "pitch": 0.19628521048909953,
+          "rotation": 0,
+          "target": "2-r0020009-30"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0020009-30",
+      "name": "R0020009-30",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.1107132357804197,
+        "pitch": 0.4159662907232171,
+        "fov": 1.3797825355761058
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.259217247942116,
+          "pitch": 0.07618269124188082,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.880606029714973,
+          "pitch": 0.1683791441525706,
+          "title": "More offices<br>",
+          "text": "<div>no photo tour here</div>"
+        }
+      ]
+    }
+  ],
+  "name": "TCS tour test-2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
